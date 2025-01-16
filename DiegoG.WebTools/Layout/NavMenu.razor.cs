0c40f11135a88faee9798f57d9b544ce14e876f2
Diego García <dagarciam1014@gmail.com>
2025-01-16 10:15:40 -0500

    Updated nav-menu to include external apps

--- DiegoG.WebTools/Layout/NavMenu.razor.cs
+++ DiegoG.WebTools/Layout/NavMenu.razor.cs
@@ -1,10 +1,22 @@
 ﻿using DiegoG.WebTools.Services;
 using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
 
 namespace DiegoG.WebTools.Layout;
 
 public partial class NavMenu
 {
+    private class AppReference
+    {
+        public string DisplayName { get; init; }
+        public string Uri { get; init; }
+        public string Id { get; init; }
+        public Dictionary<string, string>? LocalizedNames { get; init; }
+        public string? Styles { get; init; }
+    }
+
+    private List<AppReference>? Apps;
+
     [CascadingParameter]
     public LanguageProvider Language { get; set; }
 
@@ -12,6 +24,16 @@ public partial class NavMenu
     {
         base.OnInitialized();
         Language.PropertyChanged += Language_PropertyChanged;
+    }
+
+    private string GetAppDisplayName(AppReference app)
+        => app.LocalizedNames?.TryGetValue(Language.CurrentLanguage.LanguageCode, out var name) is true ? name : app.DisplayName;
+
+    protected override async Task OnInitializedAsync()
+    {
+        var response = await Http.GetAsync("https://raw.githubusercontent.com/DiegoG1019/DiegoG1019/refs/heads/main/Apps.json");
+        if (response.IsSuccessStatusCode)
+            Apps = await response.Content.ReadFromJsonAsync<List<AppReference>>();
     }
 
     private void Language_PropertyChanged(object? sender, System.ComponentModel.PropertyChangedEventArgs e)
