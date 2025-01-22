e48c3635cba2bb11bff6cf01b8eb4989e5292935
Diego García <dagarciam1014@gmail.com>
2025-01-22 11:35:25 -0500

    Updated

--- DiegoG.WebTools/Services/AppCatalog.cs
+++ DiegoG.WebTools/Services/AppCatalog.cs
@@ -17,12 +17,12 @@ public sealed class AppReference
 
 public sealed class AppCatalog(HttpClient Http)
 {
-    private ImmutableArray<AppReference>? Apps;
+    private List<AppReference>? Apps;
     private DateTime lastRefreshed;
 
-    public bool TryGetApps([MaybeNullWhen(false)] out ImmutableArray<AppReference> apps)
+    public bool TryGetApps([MaybeNullWhen(false)] out List<AppReference> apps)
     {
-        if (Apps is not ImmutableArray<AppReference> _apps || _apps.Length is 0)
+        if (Apps is not List<AppReference> _apps || _apps.Count is 0)
         {
             apps = default;
             return false;
@@ -34,9 +34,9 @@ public sealed class AppCatalog(HttpClient Http)
         }
     }
 
-    public async ValueTask<ImmutableArray<AppReference>> FetchApps()
+    public async ValueTask<List<AppReference>?> FetchApps()
     {
-        if (Apps is ImmutableArray<AppReference> arr && lastRefreshed + TimeSpan.FromMinutes(10) >= DateTime.Now)
+        if (Apps is List<AppReference> arr && lastRefreshed + TimeSpan.FromMinutes(10) >= DateTime.Now)
             return arr;
 
         using var request = new HttpRequestMessage(HttpMethod.Get, "https://raw.githubusercontent.com/DiegoG1019/DiegoG1019/refs/heads/main/Apps.json");
@@ -44,7 +44,7 @@ public sealed class AppCatalog(HttpClient Http)
         var response = await Http.SendAsync(request);
         if (response.IsSuccessStatusCode)
         {
-            var apps = await response.Content.ReadFromJsonAsync<ImmutableArray<AppReference>>();
+            var apps = await response.Content.ReadFromJsonAsync<List<AppReference>>();
             Apps = apps;
             lastRefreshed = DateTime.Now;
             return apps;
