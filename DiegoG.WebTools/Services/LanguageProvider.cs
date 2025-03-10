3fb43f6519b8a0965392684776ebb3d3fadbdef1
Diego García <dagarciam1014@gmail.com>
2025-03-10 15:45:38 -0500

    Replaced json deserialization of NumberFormatInfo with simply assigning the values

--- DiegoG.WebTools/Services/LanguageProvider.cs
+++ DiegoG.WebTools/Services/LanguageProvider.cs
@@ -42,57 +42,36 @@ public sealed class LanguageProvider : INotifyPropertyChanged
     }
 
     public static NumberFormatInfo DefaultEnglishNumberFormatInfo { get; }
-        = JsonSerializer.Deserialize<NumberFormatInfo>(
-            """"
-            {
-              "CurrencyDecimalDigits": 2,
-              "CurrencyDecimalSeparator": ".",
-              "IsReadOnly": true,
-              "CurrencyGroupSizes": [
-                3
-              ],
-              "NumberGroupSizes": [
-                3
-              ],
-              "PercentGroupSizes": [
-                3
-              ],
-              "CurrencyGroupSeparator": ",",
-              "CurrencySymbol": "$",
-              "NaNSymbol": "NaN",
-              "CurrencyNegativePattern": 0,
-              "NumberNegativePattern": 1,
-              "PercentPositivePattern": 1,
-              "PercentNegativePattern": 1,
-              "NegativeInfinitySymbol": "-\u221E",
-              "NegativeSign": "-",
-              "NumberDecimalDigits": 2,
-              "NumberDecimalSeparator": ".",
-              "NumberGroupSeparator": ",",
-              "CurrencyPositivePattern": 0,
-              "PositiveInfinitySymbol": "\u221E",
-              "PositiveSign": "\u002B",
-              "PercentDecimalDigits": 2,
-              "PercentDecimalSeparator": ".",
-              "PercentGroupSeparator": ",",
-              "PercentSymbol": "%",
-              "PerMilleSymbol": "\u2030",
-              "NativeDigits": [
-                "0",
-                "1",
-                "2",
-                "3",
-                "4",
-                "5",
-                "6",
-                "7",
-                "8",
-                "9"
-              ],
-              "DigitSubstitution": 1
-            }
-            """"
-        )!;
+        = new NumberFormatInfo()
+        {
+            CurrencyDecimalDigits = 2,
+            CurrencyDecimalSeparator = ".",
+            CurrencyGroupSizes = [ 3 ],
+            NumberGroupSizes = [ 3 ],
+            PercentGroupSizes = [ 3 ],
+            CurrencyGroupSeparator = ",",
+            CurrencySymbol = "$",
+            NaNSymbol = "NaN",
+            CurrencyNegativePattern = 0,
+            NumberNegativePattern = 1,
+            PercentPositivePattern = 1,
+            PercentNegativePattern = 1,
+            NegativeInfinitySymbol = "-\u221E",
+            NegativeSign = "-",
+            NumberDecimalDigits = 2,
+            NumberDecimalSeparator = ".",
+            NumberGroupSeparator = ",",
+            CurrencyPositivePattern = 0,
+            PositiveInfinitySymbol = "\u221E",
+            PositiveSign = "\u002B",
+            PercentDecimalDigits = 2,
+            PercentDecimalSeparator = ".",
+            PercentGroupSeparator = ",",
+            PercentSymbol = "%",
+            PerMilleSymbol = "\u2030",
+            NativeDigits = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ],
+            DigitSubstitution = DigitShapes.None
+        };
 
     public event PropertyChangedEventHandler? PropertyChanged;
 }
